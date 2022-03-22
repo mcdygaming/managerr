00d3156b492f3bc901bdfcccbcd28e4230b57add
『TØNIC』 乂 ₭ILLΣR <57848595+Tonic990@users.noreply.github.com>
2022-03-22 16:12:21 +0700

    Update nulis.py

--- PrimeMega/modules/nulis.py
+++ PrimeMega/modules/nulis.py
@@ -4,7 +4,7 @@ from PIL import Image, ImageDraw, ImageFont
 from PrimeMega.events import register, text_set
 from PrimeMega.modules.language import gs
 
-@register(pattern="^/nulis(?: |$)(.*)")
+@register(pattern="^/nulis1(?: |$)(.*)")
 async def writer(event):
     if event.reply_to:
         k = await event.reply ("Sedang Memproses..")
@@ -15,6 +15,31 @@ async def writer(event):
     else:
         return await k.edit("Berikan Beberapa Teks")
     img = Image.open("PrimeMega/resources/bahan/kertas/kertas.jpg")
+    draw = ImageDraw.Draw(img)
+    font = ImageFont.truetype("PrimeMega/resources/bahan/font/assfont.ttf", 30)
+    x, y = 150, 140
+    lines = text_set(text)
+    line_height = font.getsize("hg")[1]
+    for line in lines:
+        draw.text((x, y), line, fill=(1, 22, 55), font=font)
+        y = y + line_height - 5
+    file = "toni.jpg"
+    img.save(file)
+    await event.reply(file=file)
+    os.remove(file)
+    await k.delete()
+    
+@register(pattern="^/nulis2(?: |$)(.*)")
+async def writer(event):
+    if event.reply_to:
+        k = await event.reply ("Sedang Memproses..")
+        reply = await event.get_reply_message()
+        text = reply.message
+    elif event.pattern_match.group(1).strip():
+        text = event.text.split(maxsplit=1)[1]
+    else:
+        return await k.edit("Berikan Beberapa Teks")
+    img = Image.open("PrimeMega/resources/bahan/kertas/bahan_1.jpg")
     draw = ImageDraw.Draw(img)
     font = ImageFont.truetype("PrimeMega/resources/bahan/font/assfont.ttf", 30)
     x, y = 150, 140
