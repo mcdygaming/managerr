6376e90a458d9c39fea280d454f1f6279a662a95
『TØNIC』 乂 ₭ILLΣR <57848595+Tonic990@users.noreply.github.com>
2022-04-07 20:57:52 +0700

    typo

--- PrimeMega/modules/chatbot.py
+++ PrimeMega/modules/chatbot.py
@@ -4,7 +4,7 @@ import aiohttp
 from googletrans import Translator as google_translator
 from pyrogram import filters
 from PrimeMega.ex_plugins.chatbot import add_chat, get_session, remove_chat
-from PrimeMega.utils.pluginshelper import admins_only, edit_or_reply
+from PrimeMega.utils.pluginhelper import admins_only, edit_or_reply
 from PrimeMega import pbot as Prime, arq, BOT_ID
 from PrimeMega.modules.language import gs
 
