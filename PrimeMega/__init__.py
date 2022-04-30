45512503e6e0912cd44405809ca808bdf1e16977
ᴛᴏɴɪ - ᴇx <103916927+Toni880@users.noreply.github.com>
2022-04-30 09:50:43 +0700

    Update __init__.py

--- PrimeMega/__init__.py
+++ PrimeMega/__init__.py
@@ -173,6 +173,7 @@ else:
     CERT_PATH = Config.CERT_PATH
     API_ID = Config.API_ID
     API_HASH = Config.API_HASH
+    BOT_ID = Config.BOT_ID
     ERROR_LOGS = Config.ERROR_LOGS
     DB_URL = Config.SQLALCHEMY_DATABASE_URI
     MONGO_DB_URI = Config.MONGO_DB_URI
