0ae655074611bb8d4275d3700e4d041964fd8372
ᴛᴏɴɪ - ᴇx <103916927+Toni880@users.noreply.github.com>
2022-04-30 10:05:53 +0700

    Update __init__.py

--- PrimeMega/modules/db/__init__.py
+++ PrimeMega/modules/db/__init__.py
@@ -5,15 +5,15 @@ from motor.motor_asyncio import AsyncIOMotorClient
 from odmantic import AIOEngine
 from pymongo import MongoClient, collection
 
-from PrimeMega import MONGO_DB, MONGO_PORT, MONGO_URI
+from PrimeMega import MONGO_DB, MONGO_PORT, MONGO_DB_URI
 
 # MongoDB Client
-mongodb = MongoClient(MONGO_URI, 27017)[MONGO_DB]
-motor = motor_asyncio.AsyncIOMotorClient(MONGO_URI)
+mongodb = MongoClient(MONGO_DB_URI, 27017)[MONGO_DB]
+motor = motor_asyncio.AsyncIOMotorClient(MONGO_DB_URI)
 db = motor[MONGO_DB]
 engine = AIOEngine(motor, MONGO_DB)
-DB_CLIENT = MongoClient(MONGO_URI)
-_DB = DB_CLIENT["Fumika"]
+DB_CLIENT = MongoClient(MONGO_DB_URI)
+_DB = DB_CLIENT["Prime Mega"]
 
 
 def get_collection(name: str) -> collection:
